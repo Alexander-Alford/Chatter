a7a4d7869e67314ed2c329f8ef327f82ee96c300
Alexander-Alford <AlexAlford14@yahoo.com>
2021-12-05 23:53:57 -0600

    Beginning restructuring project without C#/asp.net.

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ChatComponent } from './chat/chat.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    SignInComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'sign-in', component: SignInComponent },
+      { path: 'chat', component: ChatComponent }
+    ]),
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Client/src/app/chat/chat.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+
+
+type Chatter = {
+  id: number;
+  color: string;
+  name: string;
+}
+
+type Message = {
+  content: string;
+  time: number;
+}
+
+type Chat = {
+  isChatting: boolean;
+  secondChatter: Chatter | null;
+  chatterList: Chatter[];
+  chattersOnline: number;
+  messageList: Message[];
+};
+
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+})
+export class ChatComponent {
+
+
+  public clientChatObject: Chat = {
+    isChatting: true,
+    secondChatter: null,
+    chatterList: [],
+    chattersOnline: 0,
+    messageList: []
+  };  
+
+}
+
+
+
