15e409a4c241af16ac9c88633e6b71cc84068095
Alexander-Alford <AlexAlford14@yahoo.com>
2021-12-17 00:57:42 -0600

    Session cookie added to sign in component.

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { ChatComponent } from './chat/chat.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+
 @NgModule({
   declarations: [
     AppComponent,
--- Client/src/app/sign-in/sign-in.component.ts
+++ Client/src/app/sign-in/sign-in.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
   selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
+  templateUrl: './sign-in.component.html'
 })
 
 
-export class SignInComponent {
+export class SignInComponent{
 
-  public usernameSubmit: string | null = null;
-  public passwordSubmit: string | null = null;
+  public usernameSubmit: string | null = "TEST";
+  public passwordSubmit: string | null = "1234";
   public isLoggedIn: boolean = false;
+
+
 
   public async postUser(user: string|null, pass: string|null, selector: "MAKE"|"LOGIN"){
   
@@ -22,15 +23,25 @@ export class SignInComponent {
       Selector: selector
     };
 
-    fetch("http://127.0.0.1:500/api/signin", 
+    let responseJSON: {result: string, detail: string} | null = null;
+
+    await fetch("http://127.0.0.1:500/api/signin", 
     {
       method: 'POST',
       body: JSON.stringify(request),
       headers: {'Content-Type': 'application/json; charset=UTF-8'} 
     })
     .then((response) => response.json())
-    .then((sendObj) => { console.log('This is a test: ', sendObj); })
-    .catch((error) => { console.log('Error: ', error); });
+    .then((data) => responseJSON = data)
+
+    if(selector === "LOGIN"){
+      if(responseJSON!.result === "success")
+      {
+        document.cookie = "SessToken=" + responseJSON!.detail + "; path=/"
+      }
+    }
+      
+
   }
 
   public async deleteUser(user: string|null, credential: string|null, selector: "DELETE"|"LOGOUT"){
@@ -41,6 +52,8 @@ export class SignInComponent {
       Selector: selector
     };
 
+    let responseJSON: {result: string, detail: string} | null = null;
+
     fetch("http://127.0.0.1:500/api/signin", 
     {
       method: 'DELETE',
@@ -48,8 +61,16 @@ export class SignInComponent {
       headers: {'Content-Type': 'application/json; charset=UTF-8'} 
     })
     .then((response) => response.json())
-    .then((sendObj) => { console.log('This is a test: ', sendObj); })
-    .catch((error) => { console.log('Error: ', error); });
+    //.then((sendObj) => { console.log('This is a test: ', sendObj); })
+    //.catch((error) => { console.log('Error: ', error); });
+    if(responseJSON!.result === "success"){
+      document.cookie = "SessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+    }
   }
+
+  //public createSessionCookie(inputToken: string|null){
+  //  document.cookie = "SessToken=" + inputToken
+  //}
+
 
 }
